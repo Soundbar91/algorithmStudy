bb04fa5f0f005ac6a9d3da63c61633b2ef367017
신관규 <soundbar91@gmail.com>
2024-06-17 18:46:01 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 45476 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) queue.add(i);
+
+        System.out.print(solve(N, queue));
+        br.close();
+    }
+
+    public static int solve(int N, Queue<Integer> queue) {
+        while (queue.size() != 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        return queue.poll();
+    }
+}
