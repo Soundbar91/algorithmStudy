83f3876c2e366cdab9b83249aa089720d4de57ce
신관규 <soundbar91@gmail.com>
2024-06-24 13:54:56 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 440 ms, Memory: 47300 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,12 +1,10 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int N;
     static int M;
-    static Map<Integer, String> intKey = new HashMap<>();
+    static List<String> intKey = new ArrayList<>();
     static Map<String, Integer> stringKey = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
@@ -19,19 +17,15 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
-
-            intKey.put(i + 1, str);
+            intKey.add(str);
             stringKey.put(str, i + 1);
         }
 
-        while (M-- > 0) {
+        for (int i = 0; i < M; i++) {
             String input = br.readLine();
-
-            try {
-                int value = Integer.parseInt(input);
-                sb.append(intKey.get(value)).append('\n');
-
-            } catch (NumberFormatException e) {
+            if (Character.isDigit(input.charAt(0))) {
+                sb.append(intKey.get(Integer.parseInt(input) - 1)).append('\n');
+            } else {
                 sb.append(stringKey.get(input)).append('\n');
             }
         }
