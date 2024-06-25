387211a13718b6b1eb9f8984ad3ad16db83ee1fc
신관규 <soundbar91@gmail.com>
2024-06-25 13:30:29 +0900

    [Silver II] Title: 생태학, Time: 592 ms, Memory: 83988 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,28 +1,27 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
-    static int count;
-    static String tree;
-    static Map<String, Integer> map = new HashMap<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new HashMap<>();
+        int count = 0;
+        String tree;
 
         while ((tree = br.readLine()) != null && !tree.isEmpty()) {
-            count++;
             map.put(tree, map.getOrDefault(tree, 0) + 1);
+            count++;
         }
-
-        List<String> keySet = new ArrayList<>(map.keySet());
-        keySet.sort(null);
+        
+        Set<String> keySet = new TreeSet<>(map.keySet());
+        StringBuilder sb = new StringBuilder();
 
         for (String key : keySet) {
-            System.out.println(key + ' ' + String.format("%.4f", (double)map.get(key) / count * 100));
+            double percentage = (double) map.get(key) / count * 100;
+            sb.append(key).append(' ').append(String.format("%.4f", percentage)).append('\n');
         }
+
+        System.out.print(sb);
         br.close();
     }
 }
