b7dd254980abf585fa47c70c661a9c93cfcecc8f
신관규 <soundbar91@gmail.com>
2024-06-29 09:38:30 +0900

    [Silver II] Title: 스택 수열, Time: 340 ms, Memory: 27720 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,31 +8,37 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] nums = new int[n];
+        int standard = 0;
 
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(br.readLine());
+            if (nums[i] == n) standard = i;
         }
 
-        System.out.print(solve(nums, n));
+        System.out.print(valid(standard, nums) ? solve(nums, standard, n) : "NO");
         br.close();
     }
 
-    public static String solve(int[] nums, int n) {
+    public static boolean valid(int standard, int[] nums) {
+        return IntStream.range(standard + 1, nums.length - 1).noneMatch(i -> nums[i] < nums[i + 1]);
+    }
+
+    public static String solve(int[] nums, int standard, int n) {
         StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         int index = 0;
 
         for (int i = 1; i <= n; i++) {
             stack.push(i);
-            sb.append('+').append('\n');
+            sb.append("+\n");
 
             while (!stack.isEmpty() && stack.peek() == nums[index]) {
-                stack.pop();
-                sb.append('-').append('\n');
                 index++;
+                stack.pop();
+                sb.append("-\n");
             }
         }
-
+        
         if (index != n) {
             return "NO";
         }
