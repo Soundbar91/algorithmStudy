aecfbc6fd331b4f433c7f59862567e7ca79efdb9
신관규 <soundbar91@gmail.com>
2024-06-30 20:55:33 +0900

    [Silver II] Title: N과 M (10), Time: 116 ms, Memory: 14540 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] nums;
+    static boolean[] visited;
+    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        nums = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        solve(0);
+        System.out.print(sb);
+        br.close();
+    }
+
+    public static void solve(int index) {
+        if (list.size() == M) {
+            for (int value : list) sb.append(value).append(" ");
+            sb.append('\n');
+        }
+        else {
+            int preValue = 0;
+            for (int i = index; i < N; i++) {
+                int nextValue = nums[i];
+                if (!visited[i] && preValue != nextValue) {
+                    visited[i] = true;
+                    list.add(nextValue);
+                    preValue = nextValue;
+                    solve(i + 1);
+                    visited[i] = false;
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+    }
+}
