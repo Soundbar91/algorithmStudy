0439229aa0af7d3603f023825a09429385ac32c7
신관규 <soundbar91@gmail.com>
2024-06-30 20:20:41 +0900

    [Silver III] Title: N과 M (7), Time: 644 ms, Memory: 143660 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] nums;
+    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        solve();
+        System.out.print(sb);
+        br.close();
+    }
+
+    public static void solve() {
+        if (list.size() == M) {
+            for (int value : list) sb.append(value).append(" ");
+            sb.append('\n');
+        }
+        else {
+            for (int i = 0; i < N; i++) {
+                list.add(nums[i]);
+                solve();
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
