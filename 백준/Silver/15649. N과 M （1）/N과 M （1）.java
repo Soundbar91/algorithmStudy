8847e098faf27b6890a195382ce6bba08ee39bc8
신관규 <soundbar91@gmail.com>
2024-06-30 14:37:23 +0900

    [Silver III] Title: N과 M (1), Time: 1804 ms, Memory: 60676 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    static List<Integer> list = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        solve();
+        br.close();
+    }
+
+    public static void solve() {
+        if (list.size() == M) {
+            for (int value : list) System.out.print(value + " ");
+            System.out.println();
+        }
+        else {
+            for (int i = 1; i <= N; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    list.add(i);
+                    solve();
+
+                    visited[i] = false;
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+    }
+}
