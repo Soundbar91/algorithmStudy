c54bade775cb9a993ef48391767c9fcd60a2129b
신관규 <soundbar91@gmail.com>
2024-07-01 13:53:50 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 164 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, result;
-    static List<Egg> eggs = new LinkedList<>();
+    static Egg[] eggs;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
+        eggs = new Egg[N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int W = Integer.parseInt(st.nextToken());
-            eggs.add(new Egg(S, W));
+            eggs[i] = new Egg(S, W);
         }
 
         solve(0, 0);
@@ -29,40 +28,34 @@ public class Main {
 
     public static void solve(int depth, int breakEgg) {
         if (depth == N) {
-            update();
-            return ;
+            result = Math.max(result, breakEgg);
+            return;
         }
-        Egg cur = eggs.get(depth);
-        int curS = cur.S;
 
-        if (breakEgg == N - 1 || curS <= 0) solve(depth + 1, breakEgg);
-        else {
-            for (int i = 0; i < N; i++) {
-                if (i == depth) continue;
+        Egg cur = eggs[depth];
 
-                Egg next = eggs.get(i);
-                int nextS = next.S;
-                if (nextS <= 0) continue;
-
-                cur.S -= next.W;
-                next.S -= cur.W;
-
-                int temp = breakEgg;
-                if (cur.S <= 0) temp++;
-                if (next.S <= 0) temp++;
-
-                solve(depth + 1, temp);
-
-                cur.S += next.W;
-                next.S += cur.W;
-            }
+        if (cur.S <= 0 || breakEgg == N - 1) {
+            solve(depth + 1, breakEgg);
+            return;
         }
-    }
 
-    public static void update() {
-        int count = 0;
-        for (Egg egg : eggs) if (egg.S <= 0) count++;
-        result = Math.max(result, count);
+        for (int i = 0; i < N; i++) {
+            if (i == depth || eggs[i].S <= 0) continue;
+
+            Egg next = eggs[i];
+
+            cur.S -= next.W;
+            next.S -= cur.W;
+
+            int newBreakEgg = breakEgg;
+            if (cur.S <= 0) newBreakEgg++;
+            if (next.S <= 0) newBreakEgg++;
+
+            solve(depth + 1, newBreakEgg);
+
+            cur.S += next.W;
+            next.S += cur.W;
+        }
     }
 
     public static class Egg {
