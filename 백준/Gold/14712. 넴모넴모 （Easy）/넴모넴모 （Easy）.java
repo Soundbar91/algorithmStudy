76d3b421587dae519cf6b4d9ac7337978d2f8bfa
신관규 <soundbar91@gmail.com>
2024-07-04 13:01:24 +0900

    [Gold V] Title: 넴모넴모 (Easy), Time: 420 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Gold/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.java
+++ 백준/Gold/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.java
@@ -15,36 +15,31 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         visited = new boolean[N][M];
 
-        solve(0, 0);
+        solve(0);
         System.out.print(result);
         br.close();
     }
 
-    public static void solve(int depth, int start) {
-        if (check(depth)) result++;
-        if (depth == N * M) return;
+    public static void solve(int depth) {
+        if (depth == N * M) {
+            result++;
+            return;
+        }
 
-        for (int i = start; i < N * M; i++) {
-            int x = i / M, y = i % M;
-            if (visited[x][y]) continue;
+        int x = depth / M;
+        int y = depth % M;
 
+        solve(depth + 1);
+
+        if (isValid(x, y)) {
             visited[x][y] = true;
-            solve(depth + 1, i + 1);
+            solve(depth + 1);
             visited[x][y] = false;
         }
     }
 
-    public static boolean check(int depth) {
-        if (depth < 3) return true;
-
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < M - 1; j++) {
-                if(visited[i][j] && visited[i][j + 1]
-                        && visited[i + 1][j] && visited[i + 1][j + 1])
-                    return false;
-            }
-        }
-
-        return true;
+    public static boolean isValid(int x, int y) {
+        if (x == 0 || y == 0) return true;
+        return !(visited[x][y - 1] && visited[x - 1][y] && visited[x - 1][y - 1]);
     }
 }
