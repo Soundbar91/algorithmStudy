c0dcccba91936b94a9a0ac00483aa7c3bd7abe66
신관규 <soundbar91@gmail.com>
2024-07-09 10:06:28 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -9,29 +9,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        solve(0, N);
+        System.out.print(solve());
     }
 
-    public static void solve (int depth, int num) {
-        if (depth != 0 && num == N) {
-            System.out.print(depth);
-            System.exit(0);
-        }
-        else {
-            String str = String.valueOf(num);
-            char right = str.charAt(str.length() - 1);
+    public static int solve() {
+        int count = 0;
+        int copy = N;
 
-            if (num < 10) str = "0" + str;
+        do {
+            N = ((N % 10) * 10) + ((N / 10 + N % 10) % 10);
+            count++;
+        } while (N != copy);
 
-            int n1 = str.charAt(0) - '0';
-            int n2 = str.charAt(1) - '0';
-            String sum = String.valueOf(n1 + n2);
-            char left = sum.charAt(sum.length() - 1);
-
-            StringBuilder sb = new StringBuilder();
-            sb.append(right).append(left);
-
-            solve(depth + 1, Integer.parseInt(sb.toString()));
-        }
+        return count;
     }
 }
