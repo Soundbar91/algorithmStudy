2e0e947418a4a33201b327b87df4f786f7fd8953
신관규 <soundbar91@gmail.com>
2024-07-09 11:26:40 +0900

    [Silver V] Title: LCM, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(((long)A * (long)B) / solve(A, B));
+        }
+        br.close();
+    }
+
+    public static int solve(int A, int B) {
+        while (B != 0) {
+            int r = A % B;
+
+            A = B;
+            B = r;
+        }
+
+        return A;
+    }
+}
