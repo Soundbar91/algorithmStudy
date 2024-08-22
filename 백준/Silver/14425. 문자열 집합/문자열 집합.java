ed85a13e89311fff1a4eb81c6e5ef7fda6b07581
신관규 <soundbar91@gmail.com>
2024-08-22 12:41:25 +0900

    [Silver IV] Title: 문자열 집합, Time: 5980 ms, Memory: 1157696 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,32 +1,71 @@
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
-    static int result;
-    static Set<String> stringSet = new HashSet<>();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        while (N-- > 0) {
-            String input = br.readLine();
-            stringSet.add(input);
-        }
-
-        while (M-- > 0) {
-            String input = br.readLine();
-            if (stringSet.contains(input)) result++;
-        }
-
-        System.out.print(result);
-        br.close();
-    }
+	static int N, M, result;
+	static Trie trie = new Trie();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		while (N-- > 0) {
+			String str = br.readLine();
+			trie.insert(str);
+		}
+		
+		while (M-- > 0) {
+			String str = br.readLine();
+			if (trie.contains(str)) result++;
+		}
+		
+		System.out.print(result);
+		br.close();
+	}
+	
+	public static class Node {
+		Map<Character, Node> child;
+		boolean isLastChar;
+		
+		public Node() {
+			child = new HashMap<Character, Node>();
+			isLastChar = false;
+		}
+	}
+	
+	public static class Trie {
+		Node root;
+		
+		public Trie() {
+			root = new Node();
+		}
+		
+		void insert(String word) {
+			Node node = root;
+			
+			for (int i = 0; i < word.length(); i++) {
+				if(!node.child.containsKey(word.charAt(i))) node.child.put(word.charAt(i), new Node());
+				node = node.child.get(word.charAt(i));
+			}
+			
+			node.isLastChar = true;
+		}
+		
+		boolean contains(String word) {
+			Node node = root;
+			
+			for (int i = 0; i < word.length(); i++) {
+				char c = word.charAt(i);
+				Node next = node.child.get(c);
+				
+				if (next == null) return false;
+				node = next;
+			}
+			return node.isLastChar;
+		}
+	}
+	
 }
