b8e907a8562f0644adc309449866f9a7b5e15980
신관규 <soundbar91@gmail.com>
2024-08-31 10:48:15 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 124 ms, Memory: 16324 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -18,85 +18,42 @@ class Main {
             System.out.print("I'm Sorry Hansoo");
             System.exit(0);
         }
-
-        System.out.print(solve());
-        br.close();
+        else {
+            System.out.print(solve());
+            br.close();
+        }
     }
 
     public static String solve() {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder front = new StringBuilder();
+        StringBuilder back = new StringBuilder();
         int len = str.length();
 
-        if (len % 2 == 0) {
-            for (int i = 0; i < alphabet.length; i++) {
-                if (alphabet[i] != 0) {
-                    int temp = alphabet[i] / 2;
-
-                    while (alphabet[i] > temp) {
-                        sb.append((char)('A' + i));
-                        alphabet[i]--;
-                    }
-                }
-            }
-
-            for (int i = alphabet.length - 1; i >= 0; i--) {
-                if (alphabet[i] != 0) {
-                    while (alphabet[i] > 0) {
-                        sb.append((char)('A' + i));
-                        alphabet[i]--;
-                    }
-                }
-            }
-        }
-        else {
-            for (int i = 0; i < alphabet.length; i++) {
-                if (alphabet[i] != 0) {
-                    int temp = alphabet[i] / 2;
-                    if (i == memo) temp++;
-
-                    while (alphabet[i] > temp) {
-                        sb.append((char)('A' + i));
-                        alphabet[i]--;
-                    }
-                }
-            }
-
-            sb.append((char)('A' + memo));
-            alphabet[memo]--;
-
-            for (int i = alphabet.length - 1; i >= 0; i--) {
-                if (alphabet[i] != 0) {
-                    while (alphabet[i] > 0) {
-                        sb.append((char)('A' + i));
-                        alphabet[i]--;
-                    }
-                }
+        for (int i = 0; i < alphabet.length; i++) {
+            while (alphabet[i] >= 2) {
+                front.append((char) ('A' + i));
+                back.insert(0, (char) ('A' + i));
+                alphabet[i] -= 2;
             }
         }
 
-        return sb.toString();
+        if (memo != -1) {
+            front.append((char) ('A' + memo));
+        }
+
+        return front.toString() + back.toString();
     }
 
     public static boolean valid() {
-        int len = str.length();
+        int oddCount = 0;
 
-        if (len % 2 == 0) {
-            for (int count : alphabet) {
-                if (count % 2 != 0) {
-                    return false;
-                }
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] % 2 != 0) {
+                oddCount++;
+                memo = i;
             }
-            return true;
         }
-        else {
-            for (int i = 0; i < alphabet.length; i++) {
-                if (alphabet[i] % 2 != 0) {
-                    if (memo != -1) return false;
-                    memo = i;
-                }
-            }
 
-            return memo != -1;
-        }
+        return oddCount <= 1;
     }
 }
