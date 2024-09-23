406fe39eebec5a79a6a66956d0af0c1516f96717
신관규 <soundbar91@gmail.com>
2024-09-23 10:47:18 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static int N, index;
+    static String pattern;
+    static String[] patterns;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        pattern = br.readLine();
+        patterns = pattern.split("\\*");
+
+        while (N-- > 0) {
+            String str = br.readLine();
+
+            System.out.println(solve(str) ? "DA" : "NE");
+        }
+
+        br.close();
+    }
+
+    public static boolean solve(String str) {
+        if (patterns[0].length() + patterns[1].length() > str.length())
+            return false;
+
+        String left = str.substring(0, patterns[0].length());
+        String right = str.substring(str.length() - patterns[1].length());
+
+        return left.equals(patterns[0]) && right.equals(patterns[1]);
+    }
+}
