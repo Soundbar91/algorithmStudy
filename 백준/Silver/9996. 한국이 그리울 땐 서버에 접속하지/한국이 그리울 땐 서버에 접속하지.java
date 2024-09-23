13472443a3bd464fd78f0e59c0cb66402dc6110c
신관규 <soundbar91@gmail.com>
2024-09-23 10:48:47 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
-    static int N, index;
+    static int N;
     static String pattern;
     static String[] patterns;
 
