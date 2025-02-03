6db562cc8bd060374e2f18751b54ecbc173ec55d
신관규 <soundbar91@gmail.com>
2025-02-03 18:59:05 +0900

    [Silver II] Title: 키로거, Time: 1052 ms, Memory: 202920 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            Stack<Character> front = new Stack<>();
+            Stack<Character> back = new Stack<>();
+            String string = br.readLine();
+
+            for (char c : string.toCharArray()) {
+                if (c == '<') {
+                    if (!front.isEmpty()) {
+                        back.add(front.pop());
+                    }
+                }
+                else if (c == '>') {
+                    if (!back.isEmpty()) {
+                        front.add(back.pop());
+                    }
+                }
+                else if (c == '-') {
+                    if (!front.isEmpty()) {
+                        front.pop();
+                    }
+                }
+                else {
+                    front.add(c);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while (!front.isEmpty()) {
+                sb.append(front.pop());
+            }
+            sb.reverse();
+            while (!back.isEmpty()) {
+                sb.append(back.pop());
+            }
+            System.out.println(sb);
+        }
+
+        br.close();
+    }
+}
