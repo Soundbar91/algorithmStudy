377ada42f5201d347d73d141de301b7829f4c6c6
신관규 <soundbar91@gmail.com>
2025-10-03 10:49:41 +0900

    [Silver IV] Title: 수 찾기, Time: 568 ms, Memory: 45236 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,46 +1,59 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int M;
+    static int[] nums;
+    static int[] targets;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] NArr = new int[N];
-        int cnt = 0;
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-
-        while(st1.hasMoreTokens()){
-            NArr[cnt] = Integer.parseInt(st1.nextToken());
-            cnt++;
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(NArr);
+        Arrays.sort(nums);
 
-        int M = Integer.parseInt(br.readLine());
-        int[] MArr= new int[M];
-        cnt = 0;
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-
-        while(st2.hasMoreTokens()){
-            MArr[cnt] = Integer.parseInt(st2.nextToken());
-            cnt++;
+        M = Integer.parseInt(br.readLine());
+        targets = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            targets[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int j : MArr) {
-            sb.append(BinarrySearch(j, NArr, 0, NArr.length - 1)).append('\n');
-        }
-        System.out.println(sb);
+        solve();
+        br.close();
     }
 
-    public static int BinarrySearch(int n, int[] arr, int low, int high){
-        if (low > high) return 0;
+    public static void solve() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            if (binarySearch(targets[i])) sb.append(1).append('\n');
+            else sb.append(0).append('\n');
+        }
+        System.out.print(sb);
+    }
 
-        int middle = (low + high) / 2;
+    public static boolean binarySearch(int target) {
+        int left = 0;
+        int right = N - 1;
 
-        if (arr[middle] == n) return 1;
-        else if (arr[middle] < n) return BinarrySearch(n, arr, middle + 1, high);
-        else return BinarrySearch(n, arr, low, middle - 1);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < target) left = mid + 1;
+            else if (nums[mid] > target) right = mid - 1;
+            else return true;
+        }
+
+        return false;
     }
 }
