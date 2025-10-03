a53666890071a558aff88fb5b631c4a94f6eb09c
신관규 <soundbar91@gmail.com>
2025-10-03 11:23:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 168 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int K, N;
+    static long[] nums;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        nums = new long[K];
+        for (int i = 0; i < K; i++) {
+            nums[i] = Long.parseLong(br.readLine());
+        }
+        Arrays.sort(nums);
+
+        System.out.print(solve());
+        br.close();
+    }
+
+    public static long solve() {
+        long left = 1;
+        long right = nums[K - 1];
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = count(mid);
+
+            if (count < N) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return right;
+    }
+
+    public static long count(long len) {
+        long count = 0;
+
+        for (int i = 0; i < K; i++) {
+            count += nums[i] / len;
+        }
+
+        return count;
+    }
+}
