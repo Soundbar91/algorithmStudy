74897a566e8aa77941f8c035b9b6cff7b3053046
신관규 <soundbar91@gmail.com>
2025-10-12 20:00:54 +0900

    [Gold IV] Title: 플로이드, Time: 492 ms, Memory: 43808 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        dist = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE / 2);
+        }
+
+        m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dist[i][i] = 0;
+        }
+
+        solve();
+        br.close();
+    }
+
+    public static void solve() {
+        floydWarshall();
+        print();
+    }
+
+    public static void floydWarshall() {
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    public static void print() {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == Integer.MAX_VALUE / 2) {
+                    System.out.print(0 + " ");
+                }
+                else  {
+                    System.out.print(dist[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
