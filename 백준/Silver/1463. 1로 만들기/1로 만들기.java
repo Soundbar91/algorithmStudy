1d01ef57a2f54fbf35f91337d12b5852c524aa58
신관규 <soundbar91@gmail.com>
2025-10-23 21:01:34 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18124 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -13,13 +13,12 @@ public class Main {
 
         X = Integer.parseInt(br.readLine());
         dp = new int[X + 1];
-        Arrays.fill(dp, 987654321);
         dp[1] = 0;
 
         for (int i = 2; i <= X; i++) {
+            dp[i] = dp[i - 1] + 1;
             if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            dp[i] = Math.min(dp[i], dp[i - 1] + 1);
         }
 
         System.out.print(dp[X]);
