df6dc55d16de1902292f25767448013110f427b7
신관규 <soundbar91@gmail.com>
2025-10-23 22:10:27 +0900

    [Silver III] Title: 계단 오르기, Time: 96 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -7,38 +7,27 @@ import java.util.StringTokenizer;
 public class Main {
     static int len;
     static int[] floor;
-    static int[][] dp;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         len = Integer.parseInt(br.readLine());
         floor = new int[len + 1];
-        dp = new int[len + 1][3];
+        dp = new int[len + 1];
 
         for (int i = 1; i <= len; i++) {
             floor[i] = Integer.parseInt(br.readLine());
         }
-        
-        if (len == 1) {
-            System.out.println(floor[1]);
-            return;
-        } else if (len == 2) {
-            System.out.println(floor[1] + floor[2]);
-            return;
-        }
 
-        dp[1][1] = floor[1];
-        dp[1][2] = 0;
-        dp[2][1] = floor[2];
-        dp[2][2] = floor[1] + floor[2];
+        dp[1] = floor[1];
+        if (len >= 2) dp[2] = floor[1] + floor[2];
 
         for (int i = 3; i <= len; i++) {
-            dp[i][1] = floor[i] + Math.max(dp[i - 2][1], dp[i - 2][2]);
-            dp[i][2] = floor[i] + dp[i - 1][1];
+            dp[i] = floor[i] + Math.max(dp[i - 2], dp[i - 3] + floor[i - 1]);
         }
-        
-        System.out.print(Math.max(dp[len][1], dp[len][2]));
+
+        System.out.print(dp[len]);
         br.close();
     }
 }
