5702ed28d0551dcc9a209335b7aac4526ae54576
신관규 <soundbar91@gmail.com>
2025-11-05 12:02:29 +0900

    [Gold V] Title: 퇴사 2, Time: 680 ms, Memory: 322188 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static long result = -1;
+    static int[] T;
+    static int[] P;
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        T = new int[N + 1];
+        P = new int[N + 1];
+        dp = new long[N + 2];
+
+        StringTokenizer st;
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (i + T[i] <= N + 1) {
+                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+
+        for (int i = 1; i <= N + 1; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
