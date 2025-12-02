e46fea09d165e9638f72a9a74e500c8a23e4d791
신관규 <soundbar91@gmail.com>
2025-12-02 09:43:34 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 14588 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.print(dp[N]);
+        br.close();
+    }
+}
