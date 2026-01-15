246dd6e13571bdf72f7b2c85699ffc2cac61c13b
신관규 <soundbar91@gmail.com>
2026-01-15 15:08:10 +0900

    [Silver IV] Title: 차집합, Time: 1268 ms, Memory: 143972 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -39,7 +39,7 @@ public class Main {
         int count = 0;
 
         for (int i = 0; i < nA; i++) {
-            if (Arrays.binarySearch(B, A[i]) < 0) {
+            if (!binarySearch(B, A[i])) {
                 count++;
                 sb.append(A[i]).append(" ");
             }
@@ -51,5 +51,24 @@ public class Main {
             System.out.println(count);
             System.out.print(sb);
         }
+    }
+
+    public static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
     }
 }
