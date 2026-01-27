b148c6e33290e98078838cd003ad1e08be0397c2
신관규 <soundbar91@gmail.com>
2026-01-27 09:32:30 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,34 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
-class Main {
-    static String str;
+public class Main {
+
+    static int one, zero;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        str = br.readLine();
 
-        System.out.print(solve());
-        br.close();
-    }
+        String S = br.readLine();
 
-    public static int solve() {
-        int zero = 0, one = 0;
-
-        if (str.charAt(0) == '0') zero++;
-        else one++;
-
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) != str.charAt(i + 1)) {
-                if (str.charAt(i + 1) == '0') zero++;
-                else one++;
+        for (int i = 1; i < S.length(); i++) {
+            if (i == S.length() - 1) {
+                if (S.charAt(i) == '0') {
+                    zero++;
+                } else if  (S.charAt(i) == '1') {
+                    one++;
+                }
+            }
+            
+            if (S.charAt(i) != S.charAt(i - 1)) {
+                if (S.charAt(i - 1) == '0') {
+                    zero++;
+                } else if (S.charAt(i - 1) == '1') {
+                    one++;
+                }
             }
         }
 
-        if (zero == 0 && one == 0) return 0;
-        else return Math.min(one, zero);
+        System.out.print(Math.min(one, zero));
+        br.close();
     }
 }
