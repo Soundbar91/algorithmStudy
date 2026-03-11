afa7f1681ecec09db60f42103fb20d5a411e4637
신관규 <soundbar91@gmail.com>
2026-03-11 11:31:45 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -5,24 +5,21 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static long[][] dp = new long[91][2];
+    static long[] dp = new long[91];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
 
-        dp[1][1] = 1;
-        dp[2][0] = 1;
-        dp[3][0] = 1;
-        dp[3][1] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
 
-        for (int i = 4; i <= N; i++) {
-            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-            dp[i][1] = dp[i - 1][0];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i -2];
         }
 
-        System.out.print(dp[N][0] + dp[N][1]);
+        System.out.print(dp[N]);
         br.close();
     }
 }
