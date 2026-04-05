32bc0892e1849b74e24dc8e3c318505d436d702c
신관규 <soundbar91@gmail.com>
2026-04-05 11:31:00 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10752 ms, Memory: 306260 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,62 +1,87 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    static int N, M, max;
-    static boolean[] visited;
-    static int[] result;
-    static List<Integer>[] list;
+/**
+ * 1325. 효율적인 해킹
+ * 한 번의 해킹으로 여러 개의 컴퓨터를 해킹할 수 있는 컴퓨터를 해킹
+ * A가 B를 신뢰하는 경우 -> B를 해킹하면 A도 해킹할 수 있음
+ * 한 번에 가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호 출력
+ *
+ * 방향 그래프, 모든 정점에 대해서 탐색을 돌림
+ * 탐색한 노드의 개수를 구함, 맥스값 구함
+ * -> 시간 초과
+ *
+ *
+ */
+
+public class Main {
+
+    static int N, M, max = -1;
+    static int[] counts;
+    static List<List<Integer>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        result = new int[N + 1];
-        list = new List[N + 1];
         M = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
+        counts = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-        while (M-- > 0) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
 
-            list[a].add(b);
+            int src = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+
+            graph.get(dest).add(src);
         }
 
         for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            solve(i);
+            counts[i] = bfs(i);
+            max = Math.max(max, counts[i]);
         }
 
-
-        for (int i = 1; i <= N; i++) max = Math.max(max, result[i]);
-        for (int i = 1; i <= N; i++) if (result[i] == max) sb.append(i).append(" ");
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(findMaxComputer(max));
         br.close();
     }
 
-    public static void solve(int start) {
+    public static int bfs(int src) {
         Queue<Integer> queue = new LinkedList<>();
-        visited[start] = true;
-        queue.add(start);
+        boolean[] visited = new boolean[N + 1];
+        visited[src] = true;
+        queue.add(src);
+        int count = 1;
 
         while (!queue.isEmpty()) {
             int cur = queue.poll();
-            for (int next : list[cur]) {
-                if (!visited[next]) {
-                    result[next]++;
-                    visited[next] = true;
-                    queue.add(next);
+
+            for (int next : graph.get(cur)) {
+                if (visited[next]) {
+                    continue;
                 }
+
+                visited[next] = true;
+                queue.add(next);
+                count++;
             }
         }
+
+        return count;
+    }
+
+    public static String findMaxComputer(int maxCount) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (counts[i] == maxCount) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        return sb.toString();
     }
 }
