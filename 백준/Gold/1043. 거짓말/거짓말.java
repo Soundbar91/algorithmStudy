225353dfc5f4f31ba6dd2a4c7a2efcdf66cf36d5
신관규 <soundbar91@gmail.com>
2026-04-09 14:18:20 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,12 +1,23 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 1043. 거짓말
+ * 파티에 진실을 아는 사람이 있다면 진실을 말하고, 그렇지 않는 경우에는 과장해서 말한다.
+ * 과장된 이야기를 할 수 있는 파티 개수의 최댓값을 구한다.
+ *
+ * - 파티 모두를 순회하면서 진실을 아는 사람을 업데이트 한다.
+ * - 파티 모두를 순회하면서 과장을 말할 수 있는 파티의 개수를 구한다.
+ */
+
 public class Main {
 
     static int N, M;
-    static int[] parents;
+    static boolean[] visited;
+    static boolean[] result;
+    static Queue<Integer> queue = new LinkedList<>();
     static List<List<Integer>> parties = new ArrayList<>();
-    static Set<Integer> knowTruePeoples = new HashSet<>();
+    static List<List<Integer>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,66 +26,61 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        parents = new int[N + 1];
+        visited = new boolean[M + 1];
+        result = new boolean[N + 1];
         for (int i = 0; i <= N; i++) {
-            parents[i] = i;
+            graph.add(new ArrayList<>());
         }
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i <= M; i++) {
             parties.add(new ArrayList<>());
         }
 
         st = new StringTokenizer(br.readLine());
-        int knowTruePeopleCount = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < knowTruePeopleCount; i++) {
-            knowTruePeoples.add(Integer.parseInt(st.nextToken()));
+        int knowPeopleCount = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < knowPeopleCount; i++) {
+            queue.add(Integer.parseInt(st.nextToken()));
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
-            int joinPartyPeopleCount = Integer.parseInt(st.nextToken());
-            int temp1 = Integer.parseInt(st.nextToken());
-            parties.get(i).add(temp1);
-            for (int j = 1; j < joinPartyPeopleCount; j++) {
-                int temp2 = Integer.parseInt(st.nextToken());
-                union(temp1, temp2);
-                parties.get(i).add(temp2);
+
+            int joinPeopleCount = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < joinPeopleCount; j++) {
+                int peopleNum = Integer.parseInt(st.nextToken());
+                parties.get(i).add(peopleNum);
+                graph.get(peopleNum).add(i);
             }
         }
 
-        int count = 0;
-        for (int i = 0; i < M; i++) {
-            boolean flag = false;
-            for (int peopleNum : parties.get(i)) {
-                if (knowTruePeoples.contains(find(peopleNum))) {
-                    flag = true;
-                    break;
-                }
-            }
-
-            if (!flag) {
-                count++;
-            }
-        }
-
-        System.out.print(count);
+        solve();
         br.close();
     }
 
-    public static int find(int x) {
-        if (parents[x] == x) return x;
-        else return parents[x] = find(parents[x]);
+    public static void solve() {
+        bfs();
+        countResult();
     }
 
-    public static void union(int x, int y) {
-        int X = find(x);
-        int Y = find(y);
+    public static void bfs() {
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
 
-        if (knowTruePeoples.contains(Y)) {
-            int temp = X;
-            X = Y;
-            Y = temp;
+            for (int next : graph.get(cur)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.addAll(parties.get(next));
+                }
+            }
         }
+    }
 
-        parents[Y] = X;
+    public static void countResult() {
+        int count = 0;
+        for (int i = 1; i <= M; i++) {
+            if (!visited[i]) {
+                count++;
+            }
+        }
+        System.out.print(count);
     }
 }
